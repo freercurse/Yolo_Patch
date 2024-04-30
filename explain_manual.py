2a53dd4622f565cbe656e65f21cdc64ba26d994e
freercurse <freerjoseph@yahoo.co.uk>
2024-04-30 19:59:46 +0100

    created functions to randomly decide a photo and a placement of the patch and  add it to the image

--- explain_manual.py
+++ explain_manual.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import random
 
 import numpy as np
 import cv2 as cv
@@ -15,9 +17,44 @@ LOGGER.setLevel(logging.WARNING)
 model = YOLO('model/yolov8n.pt')
 model.to('cuda')
 
-def remove_border(map):
-    print(map.shape)
+Patch_threshold = 24
+images = []
 
+#ATTACKED IMAGE INFERANCE
+
+
+
+#PATCH PLACEMENT
+def place_random(patch):
+    random_file = random.randint(0, len(images)-1)
+    image = images[random_file] 
+
+    patch_pixels = []
+    creation_counter = 0
+
+    temp = np.zeros_like(image)       
+
+    random_placement_x = random.randint(0 + Patch_threshold, image.shape[0] - Patch_threshold )
+    random_placement_y = random.randint(0 + Patch_threshold, image.shape[1] - Patch_threshold )
+
+    cv.circle(temp, (random_placement_x, random_placement_y), Patch_threshold,(255,255,255), -1)     
+
+    for x in range(0, patch.shape[0] -1):
+        for y in range(0, patch.shape[1] -1):            
+            if(patch[x][y][0] > 10):
+                patch_pixels.append(patch[x][y])
+
+    for x in range(0, temp.shape[0]):
+        for y in range(0, temp.shape[1]):            
+            if(temp[x][y][0] > 100):
+                image[x][y] = patch_pixels[creation_counter]
+                creation_counter += 1 
+
+    return image    
+
+#PATCH GENERATION
+
+def remove_border(map):    
     for x in range(0, map.shape[0] ):
         for y in range(0, map.shape[1] ):            
             if(map[x][y] <= 10):
@@ -25,17 +62,14 @@ def remove_border(map):
 
     return map
 
-#PATCH GENERATION
-
-def start_patching(map, image, thresh): 
+def start_patching(map, image): 
     unbordered = remove_border(map)
     _, _, min, _ =cv.minMaxLoc(unbordered)
 
     temp = np.zeros_like(image)
     patch = np.zeros_like(image)
 
-    cv.circle(temp, min, thresh,(255,255,255), -1)      
-    cv.imshow("temp", temp)
+    cv.circle(temp, min, Patch_threshold,(255,255,255), -1)    
 
     for x in range(0, patch.shape[0] -1):
         for y in range(0, patch.shape[1] -1):            
@@ -43,12 +77,9 @@ def start_patching(map, image, thresh):
                 patch[x][y] = image[x][y]  
 
     return patch
-    
 
 
-
-
-#PREPROCESS AND EXPLANATION GENERATION
+# IMAGE PREPROCESS 
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -59,6 +90,8 @@ def preprocess_image(image):
     tensor = transform(image)
     tensor = tensor.unsqueeze(0)  
     return tensor
+
+# EXPLANATION  PROCESSING
 
 def start_explanation(image):    
     stride = 8
@@ -79,11 +112,6 @@ def start_explanation(image):
     normalized_saliency_map = (normalized_saliency_map * 255).astype(np.uint8)
 
     return normalized_saliency_map
-
-    
-    
-
-
 
 def explain(image, window_size, stride):   
     height, width, _ = image.shape
@@ -119,13 +147,22 @@ def occlude_image(image, y, x, window_size):
 
     return occluded_image
 
+## IMAGE LOADING
+folder = 'pics/val2017'
 
+for filename in os.listdir(folder):
+    img = cv.imread(os.path.join(folder,filename))
+    if img is not None:
+            images.append(img)
+
+#PROGRAM CYCLING
 
 temp = cv.imread('pics/val2017/000000000724.jpg')
 map = start_explanation(temp)
-patch = start_patching(map, temp, 24)
+patch = start_patching(map, temp)
+attack = place_random(patch)
 
-cv.imshow("patch", patch)
+cv.imshow("Attacked Image", attack)
 cv.waitKey(0)
 cv.destroyAllWindows()
     
